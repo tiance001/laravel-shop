b174ada0f7ee5a3c5136d0477e0d7bbf6389e299
tiance001 <1340618494@qq.com>
2020-12-01 07:18:26 +0000

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    use DefaultDatetimeFormat;
     use Notifiable;
 
     /**

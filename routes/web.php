6075da94986a7c373520c385001469ed0aa36f0c
tiance001 <1340618494@qq.com>
2021-01-10 09:38:34 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 Auth::routes(['verify' => true]);
 

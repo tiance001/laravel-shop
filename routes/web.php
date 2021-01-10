276025a4d4bce386c1e0e481db8ea68796c8347e
tiance001 <1340618494@qq.com>
2021-01-10 13:54:48 +0000

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 //Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 Auth::routes(['verify' => true]);
 
@@ -17,4 +17,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+    Route::post('cart', 'CartController@add')->name('cart.add');
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
